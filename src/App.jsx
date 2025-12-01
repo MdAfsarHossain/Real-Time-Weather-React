7be98e178c081555413ed062c3185b127c735eb9
MdAfsarHossain <afsar291722@gmail.com>
2025-12-01 15:41:58 +0600

    App jsx file added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Page from "./page";
+import { FavouriteProvider, WeatherProvider } from "./provider";
+import LocationProvider from "./provider/LocationProvider";
+
+function App() {
+  return (
+    <LocationProvider>
+      <WeatherProvider>
+        <FavouriteProvider>
+          <Page />
+        </FavouriteProvider>
+      </WeatherProvider>
+    </LocationProvider>
+  );
+}
+
+export default App;
